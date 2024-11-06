40121073fa8fd2ebae8ceb772116cd840f1ae523
Christopher Blessing <74223087+Spellpunk101@users.noreply.github.com>
2024-11-06 15:09:13 -0500

    Set up chash.c

--- chash.c
+++ chash.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#include "common.h"
+#include "common_threads.h"
+#include "insert.h"
+#include "delete.h"
+#include "search.h"
 
 int main()
 {
