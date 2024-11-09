91d60b24c204ff95870b526d2e694c6d443a9d29
Christopher Blessing <74223087+Spellpunk101@users.noreply.github.com>
2024-11-08 22:33:01 -0500

    Made some structure

--- functionhash.h
+++ functionhash.h
@@ -9,9 +9,22 @@ typedef struct hash_struct
   struct hash_struct *next;
 } hashRecord;
 
-void insert()
+void insert(char name[], uint32_t salary)
 {
-
+  size_t length = strlen(name);
+  // Jenkins hash
+  size_t i = 0;
+  uint32_t hash = 0;
+  while(i != length)
+  {
+    hash += key[i++];
+    hash += hash << 10;
+    hash += hash >> 6;
+  }
+  hash += hash << 3;
+  hash ^= hash >> 11;
+  hash += hash <<15;
+  // Jenkins hash
 }
 
 void delete()
