ac976f23e823e17a27cb86022e9d47e47abffb54
Christopher Blessing <74223087+Spellpunk101@users.noreply.github.com>
2024-11-08 22:13:43 -0500

    Added Struct

--- chash.c
+++ chash.c
@@ -9,6 +9,14 @@
 #include "delete.h"
 #include "search.h"
 
+typedef struct hash_struct
+{
+  uint32_t hash;
+  char name[50];
+  uint32_t salary;
+  struct hash_struct *next;
+} hashRecord;
+
 int main()
 {
   
