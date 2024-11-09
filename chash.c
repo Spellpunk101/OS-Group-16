dfcfa84f472bdf7a80e7ccc64e8e4025c8bfecd6
Christopher Blessing <74223087+Spellpunk101@users.noreply.github.com>
2024-11-08 22:50:30 -0500

    Implemented insert

--- chash.c
+++ chash.c
@@ -15,12 +15,67 @@ typedef struct hash_struct
   struct hash_struct *next;
 } hashRecord;
 
+hashRecord *head;
+
 int main()
 {
-  hashRecord *head = malloc(sizeof(hashRecord));
-
-  
+  head = malloc(sizeof(hashRecord));
   
   free(head);
   return 0;
+}
+
+uint32_t jenkins_hash(const uint8_t* key, size_t length)
+{
+  size_t i = 0;
+  uint32_t hash = 0;
+  while(i != length)
+  {
+    hash += key[i++];
+    hash += hash << 10;
+    hash += hash >> 6;
+  }
+  hash += hash << 3;
+  hash ^= hash >> 11;
+  hash += hash <<15;
+  return hash;
+}
+
+void insert(char name[], uint32_t salary)
+{
+  uint32_t hash = jenkins_hash(name, strlen(name));
+
+  hashRecord *index = head;
+  hashRecord *backup = head;
+  int exists = 0;
+  
+  while(index != NULL)
+  {
+    if(hash == index->hash)
+    {
+      exists = 1;
+      strcpt(index->name, name);
+      index->salary = salary;
+      break;
+    }
+    backup = index;
+    index = index->next;
+  }
+  
+  if(!exists)
+  {
+    backup = malloc(sizeof(hashRecord));
+    strcpt(backup->name, name);
+    backup->salary = salary;
+  }
+}
+
+void delete()
+{
+
+}
+
+void search()
+{
+
 }
