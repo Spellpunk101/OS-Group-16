5cc9d7d6f8b8d1f0e650e762984fba20a434b09c
Christopher Blessing <74223087+Spellpunk101@users.noreply.github.com>
2024-11-08 22:21:45 -0500

    Create functionhash.h

---
+++ functionhash.h
@@ -0,0 +1,27 @@
+#ifndef __functionhash_h__
+#define __functionhash_h__
+
+typedef struct hash_struct
+{
+  uint32_t hash;
+  char name[50];
+  uint32_t salary;
+  struct hash_struct *next;
+} hashRecord;
+
+void insert()
+{
+
+}
+
+void delete()
+{
+
+}
+
+void search()
+{
+
+}
+
+#endif
