d93cb55b949179367bc0a1aafa567095cd0f43de
Sachin Sivakumar <ssk4988@gmail.com>
2024-11-10 15:36:07 -0500

    add rwlock

---
+++ rwlock.c
@@ -0,0 +1,39 @@
+// Source: https://github.com/remzi-arpacidusseau/ostep-code/blob/master/threads-sema/rwlock.c
+
+#include <semaphore.h>
+#include "rwlock.h"
+
+void rwlock_init(rwlock_t *lock)
+{
+  lock->readers = 0;
+  sem_init(&lock->lock, 0, 1);
+  sem_init(&lock->writelock, 0, 1);
+}
+
+void rwlock_acquire_readlock(rwlock_t *lock)
+{
+  sem_wait(&lock->lock);
+  lock->readers++;
+  if (lock->readers == 1)
+    sem_wait(&lock->writelock);
+  sem_post(&lock->lock);
+}
+
+void rwlock_release_readlock(rwlock_t *lock)
+{
+  sem_wait(&lock->lock);
+  lock->readers--;
+  if (lock->readers == 0)
+    sem_post(&lock->writelock);
+  sem_post(&lock->lock);
+}
+
+void rwlock_acquire_writelock(rwlock_t *lock)
+{
+  sem_wait(&lock->writelock);
+}
+
+void rwlock_release_writelock(rwlock_t *lock)
+{
+  sem_post(&lock->writelock);
+}
