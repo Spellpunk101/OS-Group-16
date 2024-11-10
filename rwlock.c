cbbc408d82e9f76ddafe1b8a0cec69bab18c4c78
Sachin Sivakumar <ssk4988@gmail.com>
2024-11-10 16:50:47 -0500

    add printing messages to rw lock

--- rwlock.c
+++ rwlock.c
@@ -1,6 +1,7 @@
-// Source: https://github.com/remzi-arpacidusseau/ostep-code/blob/master/threads-sema/rwlock.c
+// Modified from https://github.com/remzi-arpacidusseau/ostep-code/blob/master/threads-sema/rwlock.c
 
 #include <semaphore.h>
+#include <time.h>
 #include "rwlock.h"
 
 void rwlock_init(rwlock_t *lock)
@@ -16,6 +17,7 @@ void rwlock_acquire_readlock(rwlock_t *lock)
   lock->readers++;
   if (lock->readers == 1)
     sem_wait(&lock->writelock);
+  printf("%ld,READ LOCK ACQUIRED\n", time(NULL));
   sem_post(&lock->lock);
 }
 
@@ -25,15 +27,18 @@ void rwlock_release_readlock(rwlock_t *lock)
   lock->readers--;
   if (lock->readers == 0)
     sem_post(&lock->writelock);
+  printf("%ld,READ LOCK RELEASED\n", time(NULL));
   sem_post(&lock->lock);
 }
 
 void rwlock_acquire_writelock(rwlock_t *lock)
 {
   sem_wait(&lock->writelock);
+  printf("%ld,WRITE LOCK ACQUIRED\n", time(NULL));
 }
 
 void rwlock_release_writelock(rwlock_t *lock)
 {
+  printf("%ld,WRITE LOCK RELEASED\n", time(NULL));
   sem_post(&lock->writelock);
 }
